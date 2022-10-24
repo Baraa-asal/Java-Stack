1b2342cb70832613b8f63e5f696cdc358dfaa830
baraa-asal <b.aboasal@gmail.com>
2022-10-24 16:23:19 +0300

    first commit

---
+++ My First Assignment/Baraa.java
@@ -0,0 +1,7 @@
+public class Baraa {
+    public static void main(String[] args) {
+        System.out.println("My name is Bara'a Abo-Asal");
+        System.out.println("I'm 26 years old");
+        System.out.println("I'm from Tulkarm");
+    }
+}
