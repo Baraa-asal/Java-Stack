57aa6896cbd27cfc8ffd1a2c0134d87a192023b3
baraa-asal <b.aboasal@gmail.com>
2022-10-30 11:42:13 +0300

    ListOfExceptions

--- Java fundamentals/listOfExceptions/ListOfExceptions.java
+++ Java fundamentals/listOfExceptions/ListOfExceptions.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 class ListOfExceptions {
     public static void main(String[] args) {
         ArrayList<Object> myList = new ArrayList<Object>();
@@ -9,13 +10,11 @@ class ListOfExceptions {
 
         for (int i = 0; i < myList.size(); i++) {
             try {
-            Integer castedValue = (Integer) myList.get(i);
-            System.out.println(castedValue);
-        }
-        catch (ClassCastException exception) {
-            System.out.println(exception);
-
-        } 
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            } catch (ClassCastException exception) {
+                System.out.println(exception);
+            }
         }
     }
 }
