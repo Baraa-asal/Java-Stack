47157f0e404999aa42b4751d3472bd65e09ff2d7
baraa-asal <b.aboasal@gmail.com>
2022-10-30 11:40:17 +0300

    ListOfExceptions

---
+++ Java fundamentals/listOfExceptions/ListOfExceptions.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+class ListOfExceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("22");
+        myList.add("Hello");
+        myList.add("My name is baraa");
+        myList.add(11);
+
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+            Integer castedValue = (Integer) myList.get(i);
+            System.out.println(castedValue);
+        }
+        catch (ClassCastException exception) {
+            System.out.println(exception);
+
+        } 
+        }
+    }
+}
