c70e3ccf7662e40445216fbc6dc66fe20bab846f
baraa-asal <b.aboasal@gmail.com>
2022-10-31 16:48:32 +0300

    Coffee Kiosk

--- Java OOP/coffeeDore/CoffeeKiosk.java
+++ Java OOP/coffeeDore/CoffeeKiosk.java
@@ -1,5 +1,45 @@
 package coffeeDore;
 
+import java.util.ArrayList;
+
 public class CoffeeKiosk {
-    
+    private ArrayList<Item> menu;
+    private ArrayList<Order> orders;
+
+    public CoffeeKiosk() {
+        this.menu = new ArrayList<>();
+        this.orders = new ArrayList<>();
+    }
+
+    public void addMenuItem(String itemName, double itemPrice) {
+        Item item = new Item(itemName, itemPrice);
+        this.menu.add(item);
+    }
+
+    public void displayMenu() {
+        for (int i = 0; i < menu.size(); i++) {
+            System.out.println(i + " " + this.menu.get(i) + " -- " + "$" + this.menu.get(i).getPrice());
+        }
+    }
+
+    public void newOrder() {
+
+        System.out.println("Please enter customer name for new order:");
+        String name = System.console().readLine();
+
+        // My code:
+        Order order = new Order(name);
+        displayMenu();
+
+        System.out.println("Please enter a menu item index or q to quit:");
+        String itemNumber = System.console().readLine();
+
+        while (!itemNumber.equals("q")) {
+            order.addItem(this.menu.get(Integer.parseInt(itemNumber)));
+            System.out.println("Please enter a menu item index or q to quit:");
+            itemNumber = System.console().readLine();
+        }
+        order.display();
+    }
+
 }
