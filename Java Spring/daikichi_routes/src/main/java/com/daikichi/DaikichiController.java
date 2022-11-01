7b34c76914cd52dd0e809f8d76de8ce42b245383
baraa-asal <b.aboasal@gmail.com>
2022-11-01 13:04:43 +0300

    final edit of daikichi routes assignment

--- Java Spring/daikichi_routes/src/main/java/com/daikichi/DaikichiController.java
+++ Java Spring/daikichi_routes/src/main/java/com/daikichi/DaikichiController.java
@@ -37,4 +37,33 @@ public class DaikichiController {
 		return welcome;
     }
 	
+	
+	@RequestMapping("/lotto/{number}")
+    public String lotto(@PathVariable("number") int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+    }
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
